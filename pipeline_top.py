2771588c41eb0a3c866a95e56bf3c28069732878
paulbible <paulbible@10.177.159.29>
2015-01-06 16:02:17 -0500

    added top level script

---
+++ pipeline_top.py
@@ -0,0 +1,42 @@
+#!/bin/python
+
+
+import sys
+import subprocess
+
+
+def main():
+    print "Starting Pipeline"
+    print
+    print "Extracting gene regions"
+    
+    refgene_fname = "refGene.txt"
+    output_fname = "regions.tab"
+    args = ["python","generateRegions.py",refgene_fname]
+    
+    proc = subprocess.Popen(args)
+    
+    out = proc.communicate()[0]
+    
+    fout = open(output_fname,"w")
+    for line in out:
+        fout.write(line)
+    fout.close()
+    print "Done extracting regions"
+    
+    print "Extracting features in regions"
+    
+    print "training model"
+    
+    print "testing model"
+    
+    print "..."
+        
+     
+     
+    
+    
+
+
+if __name__ == "__main__":
+    main()
