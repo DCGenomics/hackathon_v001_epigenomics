bb6b733fc22d9bcec92d4013075bfb4b802bfd50
Dhruva Chandramohan <dc1340@gmail.com>
2015-01-07 12:58:57 -0500

    Created workflow folder
    Created directory for workflow scripts and preliminary skeletons

---
+++ workflow_scripts/script_skeletons/workflow2_aka_predict_skeleton.py
@@ -0,0 +1,23 @@
+#! /bin/bash
+# Expecting to get called as:
+#	annotate user bigwig model_output
+
+number_of_returned_peaks
+or:
+peak_threshold
+
+for peak in user_input:
+	#assign value/metric to peak
+	#peak_score=
+	# return peak_index, peak_data, peak_score
+	raw_output[i]=peak_index, peak_data, peak_score
+
+output=sort( raw_output)
+
+if peak_threshold is none:
+	re
+
+else:
+	output [ output$peak_score <peak_threshold,  ]  
+	return 
+
